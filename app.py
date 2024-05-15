0a1d77145f972b9aeda8464988dcf8a51fe35a83
Gustavo Dias <gustavo7dias@gmail.com>
2024-05-15 20:57:14 -0300

    Remove index and books views

--- app.py
+++ app.py
@@ -11,16 +11,6 @@ from peewee import fn, JOIN
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def index_view():
-    if request.method == "POST":
-        csv_authors = request.files.get("csv_authors")
-        wrapper_authors = TextIOWrapper(csv_authors, encoding='utf-8')
-        csv_reader = csv.reader(wrapper_authors, delimiter=',')
-        Author.insert_many(csv_reader).execute()
-
-    return render_template("pages/index.jinja")
-
 
 @app.route("/authors", methods=["GET", "POST", "PATCH", "DELETE"])
 def authors_view():
@@ -104,48 +94,6 @@ def authors_view():
     elif request.method == "DELETE":
         authors = Author.delete().execute()
         return Response(json.dumps(authors, indent=2), status=200)
-
-@app.route("/books", methods=["GET", "POST", "DELETE"])
-def books_view():
-    if request.method == "GET":
-        try:
-            page = request.args.get("page")
-
-            if page == None:
-                raise TypeError('The "page" param is required!') 
-
-            if not is_valid_number(page):
-                raise TypeError('The "page" param is an invalid number!') 
-            
-            # if page was a float string, raise ValueError
-            page = int(page)
-            offset = 10
-
-            if page < 1:
-                raise TypeError('The "page" param must be a positive integer greater then zero!') 
-            
-            books = Book.select().paginate(page, offset)
-            total_pages = math.ceil(Book.select().count() / offset)
-            books_dict = [model_to_dict(a) for a in books]
-            
-            data = {
-                "books": books_dict,
-                "total_pages": total_pages
-            }
-            data["next_page"] = page + 1 if page < total_pages else None
-            
-            return Response(json.dumps(data, indent=2), status=200)
-
-        except TypeError as e:
-            return Response(str(e), status=400)
-        
-        except ValueError as e:
-            msg = 'The "page" param must be a positive integer greater then zero!'
-            return Response(msg, status=400)
-
-    elif request.method == "DELETE":
-        books = Book.delete().execute()
-        return Response(json.dumps(books, indent=2), status=200)
     
 @app.route("/book", methods=["GET", "POST", "PATCH", "DELETE"])
 def book_view():
